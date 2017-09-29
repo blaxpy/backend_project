45974c2687caef07a80fe870d0cdf364cad7f83d
Denis Savran <denis.savr@mail.ru>
2017-09-29 15:45:28 +0300

    Extended InputDataForm's input validation

--- json_processor/forms.py
+++ json_processor/forms.py
@@ -18,5 +18,19 @@ class InputDataForm(forms.ModelForm):
             data_array_json_string = data_array.replace("'", '"')
             json_data = json.loads(data_array_json_string)
         except json.JSONDecodeError:
-            raise forms.ValidationError("Input does not match json format")
+            raise forms.ValidationError('Input does not match required format: input is not a valid JSON')
+        if isinstance(json_data, list):
+            for item in json_data:
+                try:
+                    if not (isinstance(item, dict) and len(item) == 2 and isinstance(item['a'], int) and isinstance(
+                            item['b'], int)):
+                        raise forms.ValidationError('Input does not match required format: '
+                                                    'list items must be of (dict) type '
+                                                    'with exactly two elements of (int) type')
+                except KeyError as e:
+                    raise forms.ValidationError(
+                        'Input does not match required format: in dictionary %s key "%s" doesn\'t exist' % (
+                            repr(item), e.args[0]))
+        else:
+            raise forms.ValidationError('Input does not match required format: input must be of (list) type')
         return json_data
