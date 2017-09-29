a49322afe536a260063571ae4c726630801915fe
Denis Savran <denis.savr@mail.ru>
2017-09-29 22:35:49 +0300

    Renamed last_test_request to test_request, added ShowTestInfoView, configured urls

--- json_processor/views.py
+++ json_processor/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import redirect
 from django.views import generic
 from django.contrib import messages
 
-from .models import InputData, TestInfo
+from .models import InputData, TestInfo, TestException
 from .forms import InputDataForm, StartTestForm
 
 from .tasks import test_func
@@ -37,19 +37,41 @@ class StartTestView(generic.FormView, generic.ListView):
         input_data = InputData.objects.all()
         last_test_info = TestInfo.objects.last()
         if last_test_info:
-            last_test_request = last_test_info.test_request + 1
+            test_request = last_test_info.test_request + 1
         else:
-            last_test_request = 1
+            test_request = 1
 
         for obj in input_data:
             json_request = repr(obj.data_array).replace("'", '"')
-            result_object = test_func.delay(json_request=json_request, test_request=last_test_request,
+            result_object = test_func.delay(json_request=json_request, test_request=test_request,
                                             input_data_id=obj.id)
             json_response = result_object.get()
             result = json.loads(json_response)
-            test_info = TestInfo.objects.create(test_request=last_test_request, input_data=obj, result=result)
+            test_info = TestInfo.objects.create(test_request=test_request, input_data=obj, result=result)
             test_info.save()
 
         messages.success(self.request, 'We are testing the function on the previously listed data sets. '
                                        'Wait a moment and refresh this page.')
-        return redirect('index')
+        return redirect('json_processor:show_test_info', test_request)
+
+
+class ShowTestInfoView(generic.TemplateView):
+    template_name = 'json_processor/show_test_info.html'
+
+    def get_context_data(self, **kwargs):
+        test_request = self.kwargs['test_request']
+        test_info = TestInfo.objects.filter(test_request=test_request)
+        test_exception = TestException.objects.filter(test_request=test_request)
+        zipped_info_list = []
+        if test_info:
+            for t_i in test_info:
+                zipped_info_list.append(zip(t_i.input_data.data_array, t_i.result))
+        exception_list = []
+        if test_exception:
+            for t_e in test_exception:
+                exception_list.append((t_e.input_data_id, t_e.input_data.data_array[t_e.array_item_index], t_e.exception_text))
+
+        kwargs['test_request'] = test_request
+        kwargs['zipped_info_list'] = zipped_info_list
+        kwargs['exception_list'] = exception_list
+        return super(ShowTestInfoView, self).get_context_data(**kwargs)
