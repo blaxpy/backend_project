fc3c47598de547a95f90d0cd82502dd5d2467f52
Denis Savran <denis.savr@mail.ru>
2017-09-29 16:35:14 +0300

    Added StartTestView view and start_test template, configured urls

--- json_processor/urls.py
+++ json_processor/urls.py
@@ -5,4 +5,5 @@ app_name = 'json_processor'
 urlpatterns =[
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^upload_data/$', views.UploadDataView.as_view(), name='upload_data'),
+    url(r'^start_test/$', views.StartTestView.as_view(), name='start_test'),
 ]
--- json_processor/views.py
+++ json_processor/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render, redirect
-from django.views import generic
+import json
 
-from json_processor.models import InputData, TestInfo
-from json_processor.forms import InputDataForm
+from django.shortcuts import redirect
+from django.views import generic
+from django.contrib import messages
+
+from .models import InputData, TestInfo
+from .forms import InputDataForm, StartTestForm
+
+from .tasks import test_func
 
 
 class IndexView(generic.TemplateView):
@@ -16,4 +21,33 @@ class UploadDataView(generic.FormView):
     def form_valid(self, form):
         input_data = form.save()
         print(input_data, repr(input_data.data_array))
+        return redirect('index')
+
+
+class StartTestView(generic.FormView, generic.ListView):
+    template_name = 'json_processor/start_test.html'
+    form_class = StartTestForm
+
+    def get_queryset(self):
+        return InputData.objects.all()
+
+    def form_valid(self, form):
+        input_data = InputData.objects.all()
+        last_test_info = TestInfo.objects.last()
+        if last_test_info:
+            last_test_request = last_test_info.test_request + 1
+        else:
+            last_test_request = 1
+
+        for obj in input_data:
+            json_request = repr(obj.data_array).replace("'", '"')
+            result_object = test_func.delay(json_request=json_request, test_request=last_test_request,
+                                            input_data_id=obj.id)
+            json_response = result_object.get()
+            result = json.loads(json_response)
+            test_info = TestInfo.objects.create(test_request=last_test_request, input_data=obj, result=result)
+            test_info.save()
+
+        messages.success(self.request, 'We are testing the function on the previously listed data sets! '
+                                       'Wait a moment and refresh this page.')
         return redirect('index')
