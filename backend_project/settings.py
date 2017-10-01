1ea952f31f8414d367235e775392fbbc83d3ba82
Denis Savran <denis.savr@mail.ru>
2017-10-01 12:51:44 +0300

    Added celery task routes for three queues

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+# from kombu import Queue
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -26,7 +26,6 @@ SECRET_KEY = '6wwp-&(8^8&^bu%qss62ao-&g=(tba59l=jfh2y41oefunlkos'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend_project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -85,7 +83,6 @@ DATABASES = {
         'PORT': '5433',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -105,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -119,14 +115,25 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
 
-# Celery config
+# Celery configuration
 CELERY_BROKER_URL = 'amqp://localhost'
 CELERY_RESULT_BACKEND = 'rpc://'
 CELERY_RESULT_PERSISTENT = False
 CELERY_TIMEZONE = 'Europe/Moscow'
+
+# CELERY_QUEUES = (
+#     Queue('read', routing_key='read'),
+#     Queue('test', routing_key='test'),
+#     Queue('write', routing_key='low'),
+# )
+
+CELERY_TASK_ROUTES = {
+    'json_processor.tasks.get_json_request': {'queue': 'read'},
+    'json_processor.tasks.test_func': {'queue': 'test'},
+    'json_processor.tasks.save_json_response_and_exc': {'queue': 'write'},
+}
